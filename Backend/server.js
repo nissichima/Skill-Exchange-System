f708e4fdc09c87346699dc399cfe4c266a425aba
Justin-Munger <justin.munger@torontomu.ca>
2024-11-22 10:47:34 -0500

    update server message

--- Backend/server.js
+++ Backend/server.js
@@ -2,4 +2,8 @@ const express = require("express")
 
 const app = express();
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.get("/", (req, res) =>{
+    res.send("Hello World")
+});
+
+app.listen(5000, () => console.log('Server running on http://localhost:5000/'));
