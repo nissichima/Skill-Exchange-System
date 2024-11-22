d75ddaed3bb2b04201eae9a5091a00f4896baf5b
Justin-Munger <justin.munger@torontomu.ca>
2024-11-22 11:03:14 -0500

    added import

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,11 @@
-const express = require("express")
+import express from "express";
 
 const app = express();
+
+const port = 5000;
 
 app.get("/", (req, res) =>{
     res.send("Hello World")
 });
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000/'));
+app.listen(port, () => console.log(`Server running on http://localhost:${port}/`));
