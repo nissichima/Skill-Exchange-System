79f172b3745aae592717ce9e52bc818fb6d7ece6
Justin-Munger <justin.munger@torontomu.ca>
2024-11-24 13:35:01 -0500

    Added email/username login

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -1,20 +1,26 @@
 import generateToken from '../../GenerateToken.js';
-import User from '../Schemas/User.js'
+import User from '../Models/User.js'
 import bcrypt from "bcryptjs"
 
 export const signup = async(req, res) =>{
     try {
-        const {firstName, lastName, username, password, confirmPassword, gender} = req.body;
+        const {firstName, lastName, username, email, password, confirmPassword, gender} = req.body;
         
         if(password !== confirmPassword){
             return res.status(400).json({error: "Passwords do not match"});
         }
 
-        const user = await User.findOne({username});
+        const enteredUsername = await User.findOne({username});
+        const enteredEmail = await User.findOne({email});
 
-        if(user){
+        if(enteredUsername){
             return res.status(400).json({error:"Username already exists"});
         }
+        if(enteredEmail){
+            return res.status(400).json({error:"Email already in use"});
+        }
+
+
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt)
@@ -23,6 +29,7 @@ export const signup = async(req, res) =>{
             firstName,
             lastName,
             username,
+            email,
             password: hashedPassword,
             gender
         });
@@ -31,12 +38,7 @@ export const signup = async(req, res) =>{
             generateToken(newUser._id, res);
             await newUser.save();
             
-            res.status(201).json({
-                _id: newUser._id,
-                firstName: newUser.firstName,
-                lastName: newUser.lastName,
-                username: newUser.username,
-            });
+            res.status(201).json({message:"Successfully created", user})
         
         }else{
             res.status(400).json({error:"Invalid user data"})
@@ -50,23 +52,28 @@ export const signup = async(req, res) =>{
 
 export const login = async(req, res) =>{
     try {
-        const {username, password} = req.body;
-        const user = await User.findOne({username});
-        const passwordCorrect = await bcrypt.compare(password, user?.password || "");
+        const {loginInput, password} = req.body;
 
-        if(!user || !passwordCorrect){
+        //User is able to login with both their usernames and emails
+        const user = await User.findOne({
+            $or:[
+            {username: loginInput}, 
+            {email: loginInput}
+        ]});
+
+        if(!user){
             return res.status(400).json({error:"The username or password is incorrect"})
         }
+        
+        const passwordCorrect = await bcrypt.compare(password, user?.password || "");
 
-        generateToken(user._id, res);
-
-        res.status(200).json({
-            _id: user._id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            username: user.username
-        })
-
+        if(user && passwordCorrect){
+            return res.status(200).json({message:"Successfully logged in", user})
+            generateToken(user._id, res);
+        }else{
+            return res.status(400).json({error:"The username or password is incorrect"})
+        }
+        
     } catch (error) {
         console.log("Error in login controller", error.message);
         res.status(500).json({error:"Internal server error"});
