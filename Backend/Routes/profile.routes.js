f4dec93b3541dc3ec3429a839834897512cb72c7
Justin-Munger <justin.munger@torontomu.ca>
2024-11-30 17:08:02 -0500

    added /get route

--- Backend/Routes/profile.routes.js
+++ Backend/Routes/profile.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {updateProfile} from "../Controllers/profile.controller.js"
+import {updateProfile, getProfile} from "../Controllers/profile.controller.js"
 import checkId from "../Middleware/checkId.js";
 
 const router = express.Router();
 
 // Add the getProfile route
 router.patch("/update", checkId, updateProfile);
-router.get("/get", checkId, updateProfile);
+router.get("/get", checkId, getProfile);
 
 export default router;
